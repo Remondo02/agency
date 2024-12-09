7bdbf4118991cdb4e6b8367b088d6e0c4703f19d
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-12-09 11:33:38 +0900

    Ajout de Tom Select pour le select multiple

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -8,6 +8,8 @@
     <title>@yield('title') | Administration</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/tom-select@2.4.1/dist/css/tom-select.bootstrap5.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/tom-select@2.4.1/dist/js/tom-select.complete.min.js"></script>
 </head>
 
 <body>
@@ -55,6 +57,15 @@
 
         @yield('content')
     </div>
+    <script>
+        new TomSelect('select[multiple]', {
+            plugins: {
+                remove_button: {
+                    title: 'Supprimer'
+                }
+            }
+        })
+    </script>
 </body>
 
 </html>
