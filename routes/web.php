55a356c4137bd089b601e4f05142be8d58ba20da
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-12-09 16:05:22 +0900

    Page d'un bien

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\OptionController;
+use App\Http\Controllers\Admin\PropertyController as AdminPropertyController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,6 @@ Route::get('/biens', [PropertyController::class, 'index'])->name('property.index
 Route::get('/biens/{slug}-{property}', [PropertyController::class, 'show'])->name('property.show')->where(['property' => $idRegex, 'slug' => $slugRegex]);
 
 Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('property', PropertyController::class)->except(['show']);
+    Route::resource('property', AdminPropertyController::class)->except(['show']);
     Route::resource('option', OptionController::class)->except(['show']);
 });
