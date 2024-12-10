aa26c7e25e189d64c221e45ac088cacfd63761a2
Rémi Meullemeestre <remi.meullemeestre@gmail.com>
2024-12-10 10:13:48 +0900

    Demande de contact

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/biens', [PropertyController::class, 'index'])->name('property.index');
 Route::get('/biens/{slug}-{property}', [PropertyController::class, 'show'])->name('property.show')->where(['property' => $idRegex, 'slug' => $slugRegex]);
 
+Route::post('/biens/{property}/contact', [PropertyController::class, 'contact'])->name('property.contact')->where(['property' => $idRegex]);
+
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('property', AdminPropertyController::class)->except(['show']);
     Route::resource('option', OptionController::class)->except(['show']);
